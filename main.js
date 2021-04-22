b3233c302fbbdea34e4290375e04eabb483cb368
imalovelyghost <imalovelyghost13@gmail.com>
2021-04-22 18:28:27 +0200

    js main bringing data

---
+++ main.js
@@ -0,0 +1,15 @@
+//Grab the Node list
+let sectionList = document.querySelectorAll("li");
+console.log(sectionList)
+
+
+// //Turn it into data
+// sectionList.forEach(element => {
+//     let one = sectionList.element[0];
+//     let two = sectionList.element[1];
+
+//     console.log(element)
+//     console.log(one)
+//     console.log(two)
+
+// });
