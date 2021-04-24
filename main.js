5e0f1e41005a1e2f12ebd8aeff854445a4f956c6
imalovelyghost <imalovelyghost13@gmail.com>
2021-04-24 14:47:08 +0200

    empezamos a insertar las funcionalidades pertinentes

--- main.js
+++ main.js
@@ -1,64 +1,83 @@
-//Grab the Node list
-let sectionList = document.querySelectorAll(".section-1-ul-class li");
+//GLOBALS
+let itemList = document.querySelectorAll("li");
+let vanillaButton = document.getElementById("jsVanilla");
+let jqueryButton = document.getElementById("jQuery");
+let clearButton = document.getElementById("clear");
 
-//Turn it into data
-let a = sectionList[0];
-let b = sectionList[1];
-let c = sectionList[3];
-let d = sectionList[4];
-let e = sectionList[5];
-let f = sectionList[6];
-let g = sectionList[7];
-let h = sectionList[8];
-let i = sectionList[9];
-let j = sectionList[10];
-let k = sectionList[11];
-let l = sectionList[12];
-let m = sectionList[13];
 
-//Descriptions
+//Cogemos todos los elementos li
+itemList = Array.from(itemList);
 
-let aText = (a.innerHTML) + " When the HTML document has been loaded and you can manipulate it with JavaScript ";
-let bText = (b.innerHTML) + " When an HTML item has been clicked";
-let cText = (c.innerHTML) + " When an HTML item has been double clicked";
-let dText = (d.innerHTML) + " When the user presses a key on the keyboard";
-let eText = (e.innerHTML) + " When the user moves the mouse cursor";
-let fText = (f.innerHTML) + " When the user changes a value of an text input";
-let gText = (g.innerHTML) + " When an image is loaded";
-let hText = (h.innerHTML) + " When an image fails to load, (if you write an incorrect image url the loading of the image will fail)";
-let iText = (i.innerHTML) + " When a form is submitted";
-let jText = (j.innerHTML) + " When the user changes the option of a select element";
-let kText = (k.innerHTML) + " When you position the mouse over an element";
-let lText = (l.innerHTML) + " When a checkbox is checked or unchecked";
-//let mText = (m.innerHTML) + " When a ul list item is clicked, show the item that was clicked";
+//al tag  li le agregamos los eventos pertinentes
+itemList.forEach(function(myli) {
+    myli.addEventListener("mouseover", insertDataDescription);
+    myli.addEventListener("mouseout", clearDataDescription);
+    myli.addEventListener("click", selectDesiredFunction);
+    clearButton.addEventListener("click", clearDesiredFunction);
 
-//Name functions
-let afx = (a.innerHTML) + " function";
-let bfx = (b.innerHTML) + " function";
-let cfx = (c.innerHTML) + " function";
-let dfx = (d.innerHTML) + " function";
-let efx = (e.innerHTML) + " function";
-let ffx = (f.innerHTML) + " function";
-let gfx = (g.innerHTML) + " function";
-let hfx = (h.innerHTML) + " function";
-let ifx = (i.innerHTML) + " function";
-let jfx = (j.innerHTML) + " function";
-let kfx = (k.innerHTML) + " function";
-let lfx = (l.innerHTML) + " function";
 
-//Event Listeners
-a.addEventListener("mouseover", insertDataDescription);
-a.addEventListener("mouseout", clearDataDescription);
+});
 
-function insertDataDescription() {
+//Insertamos los datos en descripción, del li target
+function insertDataDescription(event) {
+    let selectedItemList = event.target;
+    var textSelectedItemList = selectedItemList.innerHTML;
+    console.log(textSelectedItemList)
+
     let descriptionList = document.querySelector(".description-list-class");
-    descriptionList.insertAdjacentHTML('beforeend', '<li class=description-text>' + `${aText}` + '</li>');
+    if (selectedItemList !== false) {
+        descriptionList.insertAdjacentHTML('beforeend', '<li class=description-text>' + `${textSelectedItemList}` + '</li>');
+    } else {
+        console.warning("Se ha encontrado un error al intentar mostrar el item seleccionado")
+    }
 }
 
-function clearDataDescription() {
+//Limpiamos los datos en descripción, del li target
+function clearDataDescription(event) {
+    let selectedItemList = event.target;
     let description = document.querySelector(".description-text");
     description.remove();
 }
 
-let vanillaButton = document.getElementById("jsVanilla");
-let jqueryButton = document.getElementById("jQuery");
+//Guardamos la funcion seleccionada en el bloque CODE
+function selectDesiredFunction() {
+    let selectedItemList = event.target;
+    var textSelectedItemList = selectedItemList.innerHTML;
+
+    let functionList = document.querySelector(".code-class");
+    if (selectedItemList !== false) {
+        functionList.insertAdjacentHTML('beforeend', '<li class=code-text>' + `${textSelectedItemList}` + '</li>');
+    } else {
+        console.warning("Se ha encontrado un error al intentar guardar el item seleccionado")
+    }
+}
+
+function clearDesiredFunction(event) {
+    let selectedItemList = event.target;
+    let selectedFunction = document.querySelector(".code-text");
+    //Intentar eliminar todos los hijos - wishlist
+    selectedFunction.remove();
+}
+
+//Boton JS Vanilla
+vanillaButton.addEventListener("click", vanillaSelected);
+// //a.addEventListener("mouseout", clearVanillaSelected);
+
+function vanillaSelected(event) {
+
+    console.log("INNN")
+}
+
+// function clearVanillaSelected() {
+//     let output = document.querySelector(".output-text");
+//     output.remove();
+// }
+
+// //START _HERE
+
+// //vanillaButton.addEventListener("click", onPushVanilla);
+
+// function onPushVanilla() {
+//     let outputList = document.querySelector(".output-class");
+//     outputList.insertAdjacentHTML('beforeend', '<li class=output-text>' + `${afx}` + '</li>');
+// }
