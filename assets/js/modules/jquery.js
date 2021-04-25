1d3d40c618cd0c05777a03e1a8182e134f718a3c
Jonathan Cedeño <jony.cede@gmail.com>
2021-04-25 21:32:37 +0200

    added functions of comparison

---
+++ assets/js/modules/jquery.js
@@ -0,0 +1,129 @@
+/* Events functions*/
+
+export function HTMLLoaded(){
+    console.log("First function jQuery");
+}
+
+export function Click(){
+}
+
+export function DoubleClick(){
+}
+
+export function KeyPressed(){
+}
+
+export function MouseMoved(){
+}
+
+export function TextChanged(){
+}
+
+export function ImgLoaded(){
+}
+
+export function ImgFailed(){
+}
+
+export function FormSubmitted(){
+}
+
+export function OptionSelectChanged(){
+}
+
+export function MouseOver(){
+}
+
+export function CheckboxChanged(){
+}
+
+export function UlListItemClicked(){
+}
+
+/* Functions functions*/
+
+export function CreateElement(){
+}
+
+export function RemoveElement(){
+}
+
+export function AppendElement(){
+}
+
+export function PrependElement(){
+}
+
+export function CreateAfterElement(){
+}
+
+export function CreateBeforeElement(){
+}
+
+export function CloneElement(){
+}
+
+export function AddClass(){
+}
+
+export function ToggleClass(){
+}
+
+export function AddDisabled(){
+}
+
+export function RemoveDisabled(){
+}
+
+export function SetDataSrc(){
+}
+
+export function RemoveDataSrc(){
+}
+
+export function HideElement(){
+}
+
+export function ShowElement(){
+}
+
+export function FadeInElement(){
+}
+
+export function FadeOutElement(){
+}
+
+export function IterateCollection(){
+}
+
+export function ChangeHref(){
+}
+
+export function AlertFirstInput(){
+}
+
+export function RemoveAllItems(){
+}
+
+export function AnimateTwoSeconds(){
+}
+
+/* Selectors functions */
+
+export function GetParent(){
+}
+
+export function GetChildren(){
+}
+
+export function GetElementsByClass(){
+}
+
+export function GetElementById(){
+}
+
+export function GetElementsByClassAndNone(){
+}
+
+export function GetOptionsOfElement(){
+}
