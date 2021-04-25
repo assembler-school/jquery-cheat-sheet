4986b2f54dae024ffa5eaf3b0d434d0e73a6ef78
imalovelyghost <imalovelyghost13@gmail.com>
2021-04-25 18:03:46 +0200

    next one should be in another branch

--- jquery.js
+++ jquery.js
@@ -10,7 +10,19 @@ function jQueryFunctionCode() {
     //Select the funtion
     switch (fjqueryData) {
         //REady - dom content load
-        case 'onLoad':
+        case 'ready':
+            ////When the HTML document has been loaded and you can manipulate it with JavaScript
+
+            $('.code-class').append('<p>$( document ).ready()</p>');
+            $('.code-class').append('<p>$( document ).ready(function() {alert( "Im ready!" );});</p>');
+            break;
+            // case 'click':
+            //     //     When an HTML item has been clicked
+            //     let codeLiz1ºstClick = document.querySelector(".code-class");
+            //     codeListClick.insertAdjacentHTML('beforeend', '<p>&ltbutton onclick="myFunction()">Click me</button&gt</p>');
+            //     codeListClick.insertAdjacentHTML('beforeend', '<p>&ltscript&gtfunction myFunction(){document.getElementById("demo").innerHTML = "Hello World";}&ltscript&gt;</p>');
+            //     break;
+        case 'eight':
             ////When the HTML document has been loaded and you can manipulate it with JavaScript
 
             $('.code-class').append('<p>&ltimg src="jquery.png" alt="jQuery" id="jqueryImg  width="30" height="30""&gt</p>');
@@ -38,26 +50,30 @@ function jQueryFunctionOutput() {
 
     //Select the funtion
     switch (outVanillaData) {
-        case 'onLoad':
+        case 'ready':
+
+            //HTML
+            $('.output-class').append('<p>$( document ).ready()</p>');
+            //JS
+            //$('.myBody').append('<script>$(".img").load("#jqueryImg"function(){alert("Image is loaded JQ")});</' + 'script>');
+            var myScript = document.createElement("script");
+            var scriptContent = document.createTextNode("$( document ).ready(function() {swal( 'Im ready!' );});");
+            myScript.appendChild(scriptContent);
+            document.body.appendChild(myScript);
+
+            break;
+        case 'eight':
 
             //HTML
             $('.output-class').append('<p class=img><img src="jquery.png" alt="jQuery" id="jqueryImg"  width="30" height="30"></p>');
             //JS
-            $('.myBody').append('<script>$(".img").load("#jqueryImg"function(){alert("Image is loaded JQ")});</' + 'script>');
+            //$('.myBody').append('<script>$( "#jqueryImg" ).load(function() {alert("Image is loaded JQ")});</' + 'script>');
+            var myScript = document.createElement("script");
+            var scriptContent = document.createTextNode("$( '#jqueryImg' ).load(function() {swal('msg hereee')});");
+            myScript.appendChild(scriptContent);
+            document.body.appendChild(myScript);
 
             break;
-            // case 'click':
-            //     let outputListClick = document.querySelector(".output-class");
-            //     //     //HTML
-            //     outputListClick.insertAdjacentHTML('beforeend', '<p><button onclick="myFunction()">Click me</button></p>');
-            //     outputListClick.insertAdjacentHTML('beforeend', '<p><p id="demo"></p></p>');
-            //     //     //JS
-            //     var myScript = document.createElement("script");
-            //     var scriptContent = document.createTextNode("function myFunction(){document.getElementById('demo').innerHTML = 'Hello World';}");
-            //     myScript.appendChild(scriptContent);
-            //     document.body.appendChild(myScript);
-
-            //     break;
         default:
             //Declaraciones ejecutadas cuando el resultado de expresión coincide con el valor2
             console.info('This is default info of the output');
