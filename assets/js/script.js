8b5905cf0a81468b5a5c04488b00e7baac2291d7
Jonathan Cedeño <jony.cede@gmail.com>
2021-04-25 21:34:03 +0200

    changed the script directory

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,58 @@
+import * as jquery from "./modules/jquery.js";
+import * as vanilla from "./modules/vanilla.js";
+
 let timeHideShow = 500;
 let currentMenu = "none";
 
 const dataEvents = [
-    "HTML document loaded",
-    "Element clicked",
-    "Element double clicked",
-    "Key pressed",
-    "Mouse cursor moved",
-    "Text input is changed",
-    "Image is loaded",
-    "Image load failed",
-    "Form is submitted",
-    "Option in a select element is changed",
-    "Mouse is over an element",
-    "Checkbox is checked and unchecked",
-    "Ul list item is shown"
+    { text: "HTML document loaded", function: jquery.HTMLLoaded},
+    { text: "Element clicked", function: jquery.Click},
+    { text: "Element double clicked", function: jquery.DoubleClick},
+    { text: "Key pressed", function: jquery.KeyPressed},
+    { text: "Mouse cursor moved", function: jquery.MouseMoved},
+    { text: "Text input is changed", function: jquery.TextChanged},
+    { text: "Image is loaded", function: jquery.ImgLoaded},
+    { text: "Image load failed", function: jquery.ImgFailed},
+    { text: "Form is submitted", function: jquery.FormSubmitted},
+    { text: "Option in a select element is changed", function: jquery.OptionSelectChanged},
+    { text: "Mouse is over an element", function: jquery.MouseOver},
+    { text: "Checkbox is checked and unchecked", function: jquery.CheckboxChanged},
+    { text: "Ul list item is shown", function: jquery.UlListItemClicked}
 ];
 
 const dataFunctions = [
-    "Create an element",
-    "Remove an element",
-    "Append an element",
-    "Prepend an element",
-    "Create an element after another one",
-    "Create an element before another one",
-    "Clone an element within another one",
-    "Add a class to an element",
-    "Remove a class of an element",
-    "Toggle a class of an element",
-    "Add a disabled attribute to a button",
-    "Remove the disabled attribute of a button",
-    "Set a data-src attribute to an image",
-    "Remove the data-src attribute of an image",
-    "Hide an element",
-    "Show an element",
-    "Fade in a element",
-    "Fade out an element",
-    "Iterate a collection of elements",
-    "Change the href attribute of the first element",
-    "Show an alert with the value of the first <input> of the page",
-    "Remove all items from a specific selector",
-    "Animate an item after 2 seconds"
+    { text: "Create an element", function: jquery.CreateElement},
+    { text: "Remove an element", function: jquery.RemoveElement},
+    { text: "Append an element", function: jquery.AppendElement},
+    { text: "Prepend an element", function: jquery.PrependElement},
+    { text: "Create an element after another one", function: jquery.CreateAfterElement},
+    { text: "Create an element before another one", function: jquery.CreateBeforeElement},
+    { text: "Clone an element within another one", function: jquery.CloneElement},
+    { text: "Add a class to an element", function: jquery.AddClass},
+    { text: "Remove a class of an element", function: jquery.RemoveClass},
+    { text: "Toggle a class of an element", function: jquery.ToggleClass},
+    { text: "Add a disabled attribute to a button", function: jquery.AddDisabled},
+    { text: "Remove the disabled attribute of a button", function: jquery.RemoveDisabled},
+    { text: "Set a data-src attribute to an image", function: jquery.SetDataSrc},
+    { text: "Remove the data-src attribute of an image", function: jquery.RemoveDataSrc},
+    { text: "Hide an element", function: jquery.HideElement},
+    { text: "Show an element", function: jquery.ShowElement},
+    { text: "Fade in a element", function: jquery.FadeInElement},
+    { text: "Fade out an element", function: jquery.FadeOutElement},
+    { text: "Iterate a collection of elements", function: jquery.IterateCollection},
+    { text: "Change the href attribute of the first element", function: jquery.ChangeHref},
+    { text: "Show an alert with the value of the first <input> of the page", function: jquery.AlertFirstInput},
+    { text: "Remove all items from a specific selector", function: jquery.RemoveAllItems},
+    { text: "Animate an item after 2 seconds", function: jquery.AnimateTwoSeconds}
 ];
 
 const dataSelectors = [
-    "Get the parent element of a certain element",
-    "Get the collection of children of a certain element",
-    "Get all the elements that have a certain class",
-    "Get an item by id",
-    "Get all the elements that have a certain class and display property of none",
-    "Get the options of a select element that are selected"
+    { text: "Get the parent element of a certain element", function: jquery.GetParent},
+    { text: "Get the collection of children of a certain element", function: jquery.GetChildren},
+    { text: "Get all the elements that have a certain class", function: jquery.GetElementsByClass},
+    { text: "Get an item by id", function: jquery.GetElementById},
+    { text: "Get all the elements that have a certain class and display property of none", function: jquery.GetElementsByClassAndNone},
+    { text: "Get the options of a select element that are selected", function: jquery.GetOptionsOfElement}
 ];
 
 $("#events-quick").on("click", function () {
@@ -139,7 +142,7 @@ function showContentEvents(){
     $("#information-column").append($("<div>").addClass("title-data-column").text("Events"));
     $("#information-column").append($("<div>").attr("id", "data-column"));
     for (let i = 0; i < dataEvents.length; i++) {
-        let element = $("<div>").addClass("links-column").attr("id", i).text(dataEvents[i]).on("click", comparisonModalEvents);
+        let element = $("<div>").addClass("links-column").attr("id", i).text(dataEvents[i].text).on("click", comparisonModalEvents);
         $("#data-column").append(element);
     }
 }
@@ -156,7 +159,7 @@ function showContentFunctions(){
     $("#information-column").append($("<div>").addClass("title-data-column").text("Functions"));
     $("#information-column").append($("<div>").attr("id", "data-column"));
     for (let i = 0; i < dataFunctions.length; i++) {
-        let element = $("<div>").addClass("links-column").attr("id", i).text(dataFunctions[i]).on("click", comparisonModalFunctions);
+        let element = $("<div>").addClass("links-column").attr("id", i).text(dataFunctions[i].text).on("click", comparisonModalFunctions);
         $("#data-column").append(element);
     }
 }
@@ -173,7 +176,7 @@ function showContentSelectors(){
     $("#information-column").append($("<div>").addClass("title-data-column").text("Selectors"));
     $("#information-column").append($("<div>").attr("id", "data-column"));
     for (let i = 0; i < dataSelectors.length; i++) {
-        let element = $("<div>").addClass("links-column").attr("id", i).text(dataSelectors[i]).on("click", comparisonModalSelectors);
+        let element = $("<div>").addClass("links-column").attr("id", i).text(dataSelectors[i].text).on("click", comparisonModalSelectors);
         $("#data-column").append(element);
     }
 }
@@ -182,20 +185,25 @@ function comparisonModalEvents(){
     $(".modal-comparison").remove();
     let element = $("#template-comparison").contents().clone();
     $("#events-column").append(element);
-    console.log("Events:" + $(this).attr("id"));
-    
+
+    let id = $(this).attr("id");
+    dataEvents[id].function();
 }
 
 function comparisonModalFunctions(){
     $(".modal-comparison").remove();
     let element = $("#template-comparison").contents().clone();
     $("#functions-column").append(element);
-    console.log("Functions:" + $(this).attr("id"));
+
+    let id = $(this).attr("id");
+    dataFunctions[id].function();
 }
 
 function comparisonModalSelectors(){
     $(".modal-comparison").remove();
     let element = $("#template-comparison").contents().clone();
     $("#selectors-column").append(element);
-    console.log("Selectors:" + $(this).attr("id"));
+
+    let id = $(this).attr("id");
+    dataSelectors[id].function();
 }
