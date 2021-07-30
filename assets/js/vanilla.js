9b500780051e691b3864214c9c8edbf4f382ab6a
AranBeitia <arantza.beitia@gmail.com>
2021-07-30 18:07:52 +0200

    accordion

--- assets/js/vanilla.js
+++ assets/js/vanilla.js
@@ -1,1 +1,8 @@
-console.log('hello kitty');
+window.onload = function() {
+  if(window.innerWidth < 480) {
+    document.querySelector('main').classList.add('accordion')
+    document.querySelector('#events').classList.add('accordion__cell')
+    document.querySelector('#functions').classList.add('accordion__cell')
+    document.querySelector('#selectors').classList.add('accordion__cell')
+  }
+}
