3899d3d0c26c01573d8c22137225bd327212b0ec
Yaiser <yaiserar@gmail.com>
2021-07-31 21:04:36 +0200

    Qs about events answered

---
+++ assets/js/vanilla.js
@@ -0,0 +1,4 @@
+//create and event when the HTML document has been loaded and you can manipulate it with JavaScript
+$(document).ready(() => {});
+document.addEventListener("DOMContentLoaded", () => {});
+//?Vanilla
