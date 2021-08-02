55047bf9d75f656337c061f8efa57f178d458e33
gonzalogarahuetes <gonzaloarahuetes@gmail.com>
2021-08-02 11:13:00 +0200

    First Structure and Style

---
+++ assets/js/templates.js
@@ -0,0 +1,31 @@
+//----------------------------------------------MAIN PAGE----------------------------------------//
+
+let main = `
+<template id="main">
+<div id="main-screen">
+    <header id="header">
+        <nav class="header__nav">
+        <a href="#main" class="header__link">Home</a>
+        <a href="#cheatList" class="header__link">Cheat List</a>
+        </nav>
+    </header>
+    <main class="main">
+        <h1 class="main__title">THE JQUERY HORROR CHEAT SHEET!!</h1>
+        <h2 class="main__subtitle">Join the forces of Good and Evil</h2>
+        <h2 class="main__subtitle">
+        (and learn some JQuery in the meantime)
+        </h2>
+        <input id="start" type="button" value="START" class="button button--scary" />
+    </main>
+</div>
+</template>
+`;
+
+//----------------------------------------------CONTENT----------------------------------------//
+
+let content = `
+<template id="content>
+</template>
+`;
+
+//----------------------------------------------CHEAT LIST----------------------------------------//
