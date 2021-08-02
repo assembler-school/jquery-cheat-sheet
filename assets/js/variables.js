6ba95679dea62f4d3c60b5c7f81cf34166a7cd06
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2021-08-02 13:54:02 +0100

    initial commit

---
+++ assets/js/variables.js
@@ -0,0 +1,20 @@
+/**
+ * Global Variables
+ */
+var objectsJS = {
+  id: "",
+  code: "",
+  infoLink: "",
+};
+
+var objectsJQ = {
+  id: "",
+  code: "",
+  infoLink: "",
+};
+
+var examples = {
+  id: "",
+  code: "",
+  popUpText: "",
+};
