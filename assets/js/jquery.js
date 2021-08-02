4dcc1159ca49e9bfd3aa5832a2e85a06032351c3
AranBeitia <arantza.beitia@gmail.com>
2021-08-02 18:31:21 +0200

    Wip events

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,1 +1,71 @@
-// $('h1').css('color', 'blue')
+// Events
+const loadHTML = $('<span>HTML loaded</span>')
+$(() => loadHTML)
+$('#eventLoadJq').on('click', () => {
+  $('#eventLoadJq').after(loadHTML)
+})
+
+const item = $('<span></span>')
+$('#eventClickJq').on('click', () => {
+  item.text('Item clicked')
+  $('#eventClickJq').after(item)
+})
+
+$('#eventDblClickJq').on('dblclick', () => {
+  let item = $('<span>Item double clicked</span>')
+  $('#eventDblClickJq').after(item)
+})
+
+const coordenates = $('<p></p>')
+$('#eventMouseMoveJc').on('mousemove', (e) => {
+  let coorX = e.clientX
+  let coorY = e.clientY
+  coordenates.text(`Coordinates: (${coorX}, ${coorY})`)
+  $('#eventMouseMoveJc').after(coordenates)
+})
+
+const coordOver = $('<p></p>')
+$('#eventMouseOverJc').on('mouseover', (e) => {
+  let coorX = e.clientX
+  let coorY = e.clientY
+  coordOver.text(`Coordinates: (${coorX}, ${coorY})`)
+  $('#eventMouseOverJc').after(coordOver)
+})
+
+const key = $('<p></p>')
+$(() => key)
+$(document).keydown((e) => {
+  key.text(`You have pressed the key ${e.key}`)
+  $('#eventKeyboardJc').after(key)
+})
+
+const submitMessage = $('<p></p>')
+$('#eventSubmitJq').on('submit', (e) => {
+  e.preventDefault()
+  let name = $('#yourNameJq').val()
+  submitMessage.text(`Hi ${name}, your form is sent correctly`)
+  $('#eventSubmitJq').after(submitMessage)
+})
+
+const result = $('<p></p>')
+$('#eventChangeJq').change(() => {
+  result.text(`Your animal is: ${$(event.target).val()}`)
+  $('#eventChangeJq').after(result)
+})
+
+const selectMessage = $('<p></p>')
+$('#eventOpSelectJq select').change(() => {
+  let option = $(event.target).val()
+  selectMessage.text(`Selected: ${option}`)
+  $('#eventOpSelectJq').after(selectMessage)
+})
+
+let checkMessage = $('<p></p>')
+$('#eventCheckJq input').on('change', () => {
+  if($(event.target).is(':checked')){
+    checkMessage.text('Is checked!!')
+  } else {
+    checkMessage.text('Not checked')
+  }
+  $('#eventCheckJq').after(checkMessage)
+})
--- assets/js/vanilla.js
+++ assets/js/vanilla.js
@@ -1,8 +1,75 @@
-window.onload = function() {
-  if(window.innerWidth < 480) {
-    document.querySelector('main').classList.add('accordion')
-    document.querySelector('#events').classList.add('accordion__cell')
-    document.querySelector('#functions').classList.add('accordion__cell')
-    document.querySelector('#selectors').classList.add('accordion__cell')
-  }
-}
+import { modal } from './modal.js'
+
+// Events
+const loadHTML = document.createElement('span')
+document.addEventListener('DOMContentLoaded' , () => loadHTML )
+
+document.getElementById('eventLoadV').addEventListener('click', (e) => {
+  loadHTML.textContent = 'HTML loaded'
+  e.target.parentNode.appendChild(loadHTML)
+})
+
+document.getElementById('eventClickV').addEventListener('click', (e) => {
+  let item = document.createElement('span')
+  item.textContent = 'Item clicked'
+  e.target.parentNode.appendChild(item)
+})
+
+document.getElementById('eventDblClickV').addEventListener('dblclick', (e) => {
+  let item = document.createElement('span')
+  item.textContent = 'Item double clicked'
+  e.target.parentNode.appendChild(item)
+})
+
+const coordinates = document.createElement('p')
+document.getElementById('eventMouseMoveV').addEventListener('mousemove', (e) => {
+  let coorX = e.clientX
+  let coorY = e.clientY
+  coordinates.innerHTML = `Coordinates: (${coorX}, ${coorY})`
+  e.target.parentNode.appendChild(coordinates)
+})
+
+const coordOver = document.createElement('p')
+document.getElementById('eventMouseOverV').addEventListener('mouseover', (e) => {
+  let coorX = e.clientX
+  let coorY = e.clientY
+  coordOver.innerHTML = `Coordinates: (${coorX}, ${coorY})`
+  e.target.parentNode.appendChild(coordOver)
+})
+
+const key = document.createElement('p')
+document.addEventListener('DOMContentLoaded', () => key )
+document.addEventListener('keypress', (e) => {
+  key.textContent = `You have pressed the key ${e.key}`
+  document.getElementById('eventKeyboardV').appendChild(key)
+})
+
+const submitMessage = document.createElement('p')
+document.getElementById('eventSubmitV').addEventListener('submit', (e) => {
+  e.preventDefault()
+  let name = document.getElementById('yourName').value
+  submitMessage.textContent = `Hi ${name}, your form is sent correctly`
+  e.target.parentNode.append(submitMessage)
+})
+
+const result = document.createElement('p')
+document.getElementById('eventChangeV').addEventListener('change', (e) => {
+  e.preventDefault()
+  result.textContent = `Your animal is: ${e.target.value}`
+  document.getElementById('eventChangeV').parentNode.appendChild(result)
+})
+
+const selectionMessage = document.createElement('p')
+document.querySelector('#eventOpSelectV select').addEventListener('change', (e) => {
+  selectionMessage.textContent = `Your selection is ${e.target.value}`
+  document.getElementById('eventOpSelectV').parentNode.appendChild(selectionMessage)
+})
+
+const checkMessage = document.createElement('p')
+document.getElementById('checkV').addEventListener('click', (e) => {
+  let checkbox = e.target.checked
+  checkbox ? checkMessage.textContent = 'Checked!!': checkMessage.textContent = 'Unchecked'
+  document.getElementById('eventCheckV').parentNode.appendChild(checkMessage)
+})
+
+
