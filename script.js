0c6eaec75d83fac861966fb763010d09d70074f9
Marcel Durgante <marceldur@gmail.com>
2021-08-05 01:43:06 +0200

    started Modal + changes to HTML and CSS

---
+++ script.js
@@ -0,0 +1,29 @@
+let targetItems = document.querySelectorAll(".ul-action--list");
+let clickedItem;
+let modalClose = document.querySelector("#close");
+let modalContainer = document.querySelector("#modal");
+let tagBody = document.querySelector(".body")
+
+
+
+/* FUNCTION OPEN MODAL WHEN LI IS CLICKED IDENTIFYING WHICH LI ID WAS CLICKED ALSO */
+
+targetItems.forEach(element => {
+    element.addEventListener('click', (e)=> {
+        if (e.target !== e.currentTarget) {
+            modalContainer.style.display = "block";
+            clickedItem = e.target.id;
+
+          } else if (e.target == e.currentTarget) {
+            return null;
+          }
+          e.stopPropagation;
+    });
+});
+
+/* FUNCTION CLOSE MODAL */
+
+modalClose.addEventListener("click", (e) => {
+  modalContainer.style.display = "none";
+
+});
