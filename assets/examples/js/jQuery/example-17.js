8adfd3ed1ec6834bb293452d4136b6f27ab24e33
sanadriu <skualed@gmail.com>
2021-08-05 09:02:41 +0200

    Fixed example 17

--- assets/examples/js/jQuery/example-17.js
+++ assets/examples/js/jQuery/example-17.js
@@ -3,11 +3,6 @@ $("#ex-17-jquery-btn").on("click", function (ev) {
 	const childElement = $("#ex-17-jquery-child");
 
 	childElement.text("This HTML Element has been prepended with jQuery JS.");
-<<<<<<< HEAD
 	childElement.prependTo(parentElement);
 	//parentElement.prepend(childElement);
-=======
-
-	parentElement.prepend(childElement);
->>>>>>> 2733413fd9d90c1bb4645b84e8d49396ca555b8d
 });
