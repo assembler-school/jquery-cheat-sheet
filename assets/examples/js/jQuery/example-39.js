1a8d53643ffb44c42b6054b302a5868b13144297
sanadriu <skualed@gmail.com>
2021-08-05 12:53:42 +0200

    Edited examples 39 and 40

--- assets/examples/js/jQuery/example-39.js
+++ assets/examples/js/jQuery/example-39.js
@@ -1,5 +1,5 @@
 $("#ex-39-jquery-btn").on("click", function (ev) {
-	const anchor = $("#ex-39-jquery-list a").eq(0);
+	const anchor = $("#ex-39-jquery-list a").first();
 	anchor.attr("href", "example-28.html");
 	anchor.text("Example-28");
 });
--- assets/examples/js/jQuery/example-40.js
+++ assets/examples/js/jQuery/example-40.js
@@ -1,4 +1,4 @@
 $("#ex-40-jquery-btn").on("click", function (ev) {
-	const anchor = $("#ex-40-jquery-list input").eq(0);
+	const anchor = $("#ex-40-jquery-list input").first();
 	alert(anchor.val());
 });
