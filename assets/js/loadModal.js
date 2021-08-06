16197ace506faab3628480802f7097cc9b5adbcf
tamtran <tamtran2885@gmail.com>
2021-08-07 00:55:04 +0200

    add snowflake effects

--- assets/js/loadModal.js
+++ assets/js/loadModal.js
@@ -40,3 +40,26 @@ window.onclick = function (event) {
         }
     })
 };
+
+
+/******* SNOWFLAKE EFFECTS********/
+
+const body = document.body
+
+setInterval(createSnowFlake, 50)
+
+function createSnowFlake() {
+    const snowFlake = document.createElement('i');
+    snowFlake.classList.add('fas');
+    snowFlake.classList.add('fa-snowflake');
+    snowFlake.style.left = Math.random() * window.innerWidth + 'px';
+    snowFlake.style.animationDuration = Math.random() * 3 + 4 + 's';
+    snowFlake.style.opacity = Math.random();
+    snowFlake.style.fontSize = Math.random() * 10 + 10 + 'px';
+
+    document.body.appendChild(snowFlake);
+
+    setTimeout(() => {
+        snowFlake.remove();
+    }, 5000)
+}
