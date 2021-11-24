736f23c75c8313e91e09cd33311aea04366a7644
Adrián Serrano <adrianno_3@hotmail.com>
2021-11-24 18:21:44 +0100

    Remove modal function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,13 +6,30 @@ console.log(optionPArr)
 
 optionPArr.forEach(element => {
     element.addEventListener("click", function() {
-        let bigDiv = document.createElement("div")
-        bigDiv.classList.add("modalTransparent")
-        $("body").append(bigDiv)
-        console.log(element.textContent)
-        let smallDiv = document.createElement("div")
-        smallDiv.classList.add("modalSmall")
-        bigDiv.appendChild(smallDiv)
-
+        createModal(element)
+        
     })
 });
+
+
+function createModal(element){
+    let bigDiv = document.createElement("div")
+    bigDiv.classList.add("modalTransparent")
+    bigDiv.setAttribute("id","modal")
+    $("body").append(bigDiv)
+    console.log(element.textContent)
+    let smallDiv = document.createElement("div")
+    smallDiv.classList.add("modalSmall")
+    bigDiv.appendChild(smallDiv)
+    removeModal(bigDiv)
+}
+
+function removeModal(child){
+    let bigDivClose = document.getElementById("modal")
+    bigDivClose.addEventListener("click",(e)=>{
+        if(e.target.matches(".modalTransparent")){
+            document.body.removeChild(child)
+            bigDivClose.removeEventListener;
+        }
+    })
+}
