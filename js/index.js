653caf4c284502cbb4c67edd9fecc95e4c9059ac
Luis Molina <luismateo2416@gmail.com>
2021-11-25 15:39:27 +0100

    part of modal structure created, close button and background created

--- js/index.js
+++ js/index.js
@@ -1,45 +1,61 @@
 //Modal creation
-function createModal(itemTitle){
-    $("body").append(`<div id="modal">
-                        <head class="">${itemTitle}</head>
-                        <main id="modal-main">
-                            <section class="vanilla">
-                                <h2>Vanilla JS</h2>
-                                <!-- posible insercion de code snippet -->
-                                <div class="demo">
-                                    <!--VanillaDemo-->
-                                </div>
-                            </section>
-                            <img src="../assets/img/modal_prog.svg" id="modal-img">
-                            <section class="jquery">
-                                <h2>Vanilla JS</h2>
-                                <!-- posible insercion de code snippet -->
-                                <div class="demo">
-                                    <!--jQueryDemo-->
-                                </div>
-                                
-                            </section>
-                        </main>
-                    </div>`)
+function createModal(obj){
+    $("body").append(`
+    
+                <div id="background-modal"></div>                  
+                    <div id="modal">
+                        <header class="">
+                            ${obj.itemGroup}
+                            <img id="exit" src="../assets/img/cross.png">
+                        </header>
+
+                        <div id="blackboard">
+                            <article>
+                            ${obj.itemTitle}
+                            </article>
+
+                            <main id="modal-main">
+                                <section class="vanilla">
+                                    <h2>Vanilla JS</h2>
+                                    <!-- posible insercion de code snippet -->
+                                    <div class="demo">
+                                        <!--VanillaDemo-->
+                                    </div>
+                                </section>
+
+                                <img src="../assets/img/modal_prog.svg" id="modal-img">
+
+                                <section class="jquery">
+                                    <h2>Vanilla JS</h2>
+                                    <!-- posible insercion de code snippet -->
+                                    <div class="demo">
+                                        <!--jQueryDemo-->
+                                    </div>
+                                </section>
+                            </main>
+                        </div>
+                    </div>
+                `)
+                //event listener if click out the modal and exit button
+                $("#exit").on("click", removeModal);
+                $("#background-modal").on("click",removeModal);
 }
 //remove modal
 function removeModal(){
-    $("#modal").remove()
+    $("#background-modal").remove();
+    $("#modal").remove();
 }
 
 
-//event listeners for paragrahs
+
+
+
+//event listeners for paragrahs when document is loaded
 
 $(function(){
     $(".item").each(function(idx,element){
         $(element).on("click",function(event){
-            if($("#modal").length){
-                removeModal();
                 createModal(event.target.textContent);
-            }else{
-                createModal(event.target.textContent);
-            }
-            
         })
     })
 })
