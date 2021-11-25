5aea6b15c9c86090224f31aa1a2baa11388f416f
Bertinoti <jeffersonbertinoti@gmail.com>
2021-11-25 15:41:47 +0100

    function done

--- assets/scripts/mainScript.js
+++ assets/scripts/mainScript.js
@@ -1,4 +1,4 @@
-function createDiv(title, idButtonClick, divdisplay, functionIdJs, functionJs, jsText,IdTestResultjs, functionIdJq, functionJq, jqText ,IdTestResultjq ) {
+function createDiv(title, idButtonClick, divdisplay, functionIdJs, functionJs, jsText, IdTestResultjs, functionIdJq, functionJq, jqText, IdTestResultjq) {
     let content = `<div class="lineItem">`
     content += `<h2>${title} </h2> <button id="${idButtonClick}" class="btn btn-secondary btn-lg" >Click to See more </button>
     </div>`
@@ -13,7 +13,7 @@ function createDiv(title, idButtonClick, divdisplay, functionIdJs, functionJs, j
     content += `<div class="firstLine"><h4>JQuery</h4>  <button id="${functionIdJq}" class="btn btn-warning btn-lg">TRY</button>`
     content += `<h5> ${jqText}</h5>`
 
-    content+= `<div id="${IdTestResultjq}" class="divTest"></div></div> `
+    content += `<div id="${IdTestResultjq}" class="divTest"></div></div> `
 
     content += `</div>`
 
@@ -21,18 +21,18 @@ function createDiv(title, idButtonClick, divdisplay, functionIdJs, functionJs, j
 
     divContent.innerHTML += content
 
-    showButton = document.getElementById(idButtonClick)
-    divdisplay= document.getElementById(divdisplay)
-    //showButton.classList.toggle("show");
-    showButton.addEventListener('click', function() {
-        divdisplay.classList.toggle("hide");
-    })
-    callFunctionjs = document.getElementById(functionIdJs)
-    callFunctionjs.addEventListener('click', functionJs)
-    console.log( callFunctionjs.addEventListener('click', functionJs))
+    // showButton = document.getElementById(idButtonClick)
+    // divdisplay= document.getElementById(divdisplay)
+    // //showButton.classList.toggle("show");
+    // showButton.addEventListener('click', function() {
+    //     divdisplay.classList.toggle("hide");
+    // })
+    // callFunctionjs = document.getElementById(functionIdJs)
+    // callFunctionjs.addEventListener('click', functionJs)
+    // console.log( callFunctionjs.addEventListener('click', functionJs))
 
-    callFunctionjq = document.getElementById(functionIdJq)
-    callFunctionjq.addEventListener('click', functionJq)
+    // callFunctionjq = document.getElementById(functionIdJq)
+    // callFunctionjq.addEventListener('click', functionJq)
 
 }
 
@@ -44,11 +44,11 @@ var allElements = [
         idButtonClick: 'createElementidButtonClick',
         divdisplay: 'createElementdivdisplay',
         functionIdJs: 'createElementfunctionIdJs',
-        functionJs:  ()=> {createElementfunctionJs()},
+        functionJs: () => { createElementfunctionJs() },
         jsText: 'el = document.createElement("span");<br>el.innerHTML = "text";',
         IdTestResultjs: 'createElementIdTestResultjs',
         functionIdJq: 'createElementfunctionIdJq',
-        functionJq: ()=> {createElementfunctionJq()},
+        functionJq: () => { createElementfunctionJq() },
         jqText: 'var el = $("&lt;span&gt; "+"text" +"&lt;span&gt;");<br> var div = $("#foo")',
         IdTestResultjq: 'createElementIdTestResultjq'
     },
@@ -57,11 +57,11 @@ var allElements = [
         idButtonClick: 'removeElementidButtonClick',
         divdisplay: 'removeElementdivdisplay',
         functionIdJs: 'removeElementfunctionIdJs',
-        functionJs:  ()=> {removeElementfunctionJs()},
+        functionJs: () => { removeElementfunctionJs() },
         jsText: 'el = document.removeElement("span");<br>el.innerHTML = "text";',
         IdTestResultjs: 'removeElementIdTestResultjs',
         functionIdJq: 'removeElementfunctionIdJq',
-        functionJq: ()=> {removeElementfunctionJq()},
+        functionJq: () => { removeElementfunctionJq() },
         jqText: 'var el = $("&lt;span&gt; "+"text" +"&lt;span&gt;");<br> var div = $("#foo")',
         IdTestResultjq: 'removeElementIdTestResultjq'
     },
@@ -69,8 +69,22 @@ var allElements = [
 
 function pepe() {
     allElements.forEach(function (elem) {
-        console.log(elem)
-        createDiv(elem.title, elem.idButtonClick, elem.divdisplay, elem.functionIdJs, elem.functionJs, elem.jsText,elem.IdTestResultjs, elem.functionIdJq, elem.functionJq, elem.jqText ,elem.IdTestResultjq )
+        createDiv(elem.title, elem.idButtonClick, elem.divdisplay, elem.functionIdJs, elem.functionJs, elem.jsText, elem.IdTestResultjs, elem.functionIdJq, elem.functionJq, elem.jqText, elem.IdTestResultjq)
+    })
+
+    allElements.forEach(function (elem) {
+        let divdisplay = document.getElementById(elem.divdisplay)
+
+        let showButton = document.getElementById(elem.idButtonClick)
+        showButton.addEventListener('click', function () {
+            divdisplay.classList.toggle("hide");
+        })
+
+        let callFunctionjs = document.getElementById(elem.functionIdJs)
+        callFunctionjs.addEventListener('click', elem.functionJs)
+
+        let callFunctionjq = document.getElementById(elem.functionIdJq)
+        callFunctionjq.addEventListener('click', elem.functionJq)
     })
 }
 
@@ -88,9 +102,9 @@ function hideFunc() {
         }
 }
 
-function  buttonTest(father) {
-    var btn= document.createElement('button')
-    btn.textContent ='Button Test'
+function buttonTest(father) {
+    var btn = document.createElement('button')
+    btn.textContent = 'Button Test'
     btn.setAttribute('id', 'button-test')
     btn.classList.add('btn')
     btn.classList.add('btn-danger')
@@ -98,6 +112,6 @@ function  buttonTest(father) {
     divtest.appendChild(btn)
 }
 
-function refreshDom(){
-    
+function refreshDom() {
+
 }
