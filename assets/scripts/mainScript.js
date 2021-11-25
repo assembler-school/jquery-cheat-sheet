af79d664cbb119e8a9c4d586cf6d0294ec903f04
Bertinoti <jeffersonbertinoti@gmail.com>
2021-11-25 09:23:51 +0100

    indent

--- assets/scripts/mainScript.js
+++ assets/scripts/mainScript.js
@@ -1,32 +1,32 @@
 function createDiv(name, jsText, jqText) {
-    let content= `<div class="lineItem">`
+    let content = `<div class="lineItem">`
     content += `<h2>${name} </h2> <button id="displayDiv" class="btn btn-secondary btn-lg" >Click to See more </button>
     </div>`
     content += `<div id='button-display' class="contentTests hide">`
-    
+
     content += `<div class="firstLine"><h4>JS</h4> <button class="btn btn-warning btn-lg">TRY</button>`
-    
-    content+=`<h5>${jsText}</h5>`
-    
-    content+=`<div id="jsTest" class="divTest"></div></div>`
-    
-    content+= `<div class="firstLine"><h4>JQ</h4>  <button class="btn btn-warning btn-lg">TRY</button><h5> ${jqText}</h5><div id="jqTest" class="divTest">
+
+    content += `<h5>${jsText}</h5>`
+
+    content += `<div id="jsTest" class="divTest"></div></div>`
+
+    content += `<div class="firstLine"><h4>JQ</h4>  <button class="btn btn-warning btn-lg">TRY</button><h5> ${jqText}</h5><div id="jqTest" class="divTest">
     <button class="btn btn-danger btn-lg"> test</button></div></div> `
-    
+
     content += `</div>`
-    
+
     divContent = document.getElementById('content')
-    
-    divContent.innerHTML= content
-    
-    showButton =document.getElementById('displayDiv')
+
+    divContent.innerHTML = content
+
+    showButton = document.getElementById('displayDiv')
     showButton.addEventListener('click', hideFunc)
-    
+
 }
 
 //createDiv('Button', 'document.createElement("button") ', '${<button/>}')
 
-var allElements= [
+var allElements = [
     {
         title: `Button`,
         jsText: `document.createElement('button')`,
@@ -35,21 +35,21 @@ var allElements= [
 ]
 
 function pepe() {
-    allElements.forEach( function (elem) {
-        createDiv( elem.title, elem.jsText, elem.jqText)
+    allElements.forEach(function (elem) {
+        createDiv(elem.title, elem.jsText, elem.jqText)
     })
 }
 
 
-function hideFunc(){
-    divToHide= document.getElementById('button-display')
+function hideFunc() {
+    divToHide = document.getElementById('button-display')
     console.log(divToHide.classList)
-    if( divToHide.classList[1] == 'show'){
+    if (divToHide.classList[1] == 'show') {
         divToHide.classList.remove('show')
         divToHide.classList.add('hide')
-    }else
-    if (divToHide.classList[1] == 'hide'){
-        divToHide.classList.remove('hide')
-        divToHide.classList.add('show')
-    }
+    } else
+        if (divToHide.classList[1] == 'hide') {
+            divToHide.classList.remove('hide')
+            divToHide.classList.add('show')
+        }
 }
