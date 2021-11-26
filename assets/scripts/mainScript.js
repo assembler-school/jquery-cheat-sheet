30bf4475b3168849ac61ae2fde0a6a0f6157f2b2
Bertinoti <jeffersonbertinoti@gmail.com>
2021-11-26 14:54:20 +0100

    functions almost done

--- assets/scripts/mainScript.js
+++ assets/scripts/mainScript.js
@@ -24,7 +24,7 @@ function createDiv(title, idButtonClick, divdisplay, functionIdJs, functionJs, j
 }
 
 
-var allSelectors = [
+var allFunctions = [
     {
         title: `Create Element`,
         idButtonClick: 'createElementidButtonClick',
@@ -44,21 +44,164 @@ var allSelectors = [
         divdisplay: 'removeElementdivdisplay',
         functionIdJs: 'removeElementfunctionIdJs',
         functionJs: () => { removeElementfunctionJs() },
-        jsText: 'el = document.removeElement("span");<br>el.innerHTML = "text";',
+        jsText: "var myobj=document.getElementById('button-test');<br> myobj.remove();",
         IdTestResultjs: 'removeElementIdTestResultjs',
         functionIdJq: 'removeElementfunctionIdJq',
         functionJq: () => { removeElementfunctionJq() },
-        jqText: 'var el = $("&lt;span&gt; "+"text" +"&lt;span&gt;");<br> var div = $("#foo")',
+        jqText: ' $("#button-test").remove();',
         IdTestResultjq: 'removeElementIdTestResultjq'
+    },
+    {
+        title: `Append Element`,
+        idButtonClick: 'appendElementidButtonClick',
+        divdisplay: 'appendElementdivdisplay',
+        functionIdJs: 'appendElementfunctionIdJs',
+        functionJs: () => { appendElementfunctionJs() },
+        jsText: " let p = document.createElement('p')<br>p.textContent= 'P append on Button' <br>btn.append(p)",
+        IdTestResultjs: 'appendElementIdTestResultjs',
+        functionIdJq: 'appendElementfunctionIdJq',
+        functionJq: () => { appendElementfunctionJq() },
+        jqText: ' var el = $("&lt;p&gt;" + "P append on Div " + "&lt;p&gt;");<br>btn.append(el);;',
+        IdTestResultjq: 'appendElementIdTestResultjq'
+    },
+    {
+        title: `Prepend  Element`,
+        idButtonClick: 'prependElementidButtonClick',
+        divdisplay: 'prependElementdivdisplay',
+        functionIdJs: 'prependElementfunctionIdJs',
+        functionJs: () => { prependElementfunctionJs() },
+        jsText: "let div = document.createElement('div')<br>let p=document.createElement('p')<br>div.prepend(p)",
+        IdTestResultjs: 'prependElementIdTestResultjs',
+        functionIdJq: 'prependElementfunctionIdJq',
+        functionJq: () => { prependElementfunctionJq() },
+        jqText: ' $("div").prepend("&lt;p&gt;Test&lt;/p&gt;");',
+        IdTestResultjq: 'prependElementIdTestResultjq'
+    },
+    {
+        title: `Create and Add  Element Before`,
+        idButtonClick: 'createandAddBeforeidButtonClick',
+        divdisplay: 'createandAddBeforedivdisplay',
+        functionIdJs: 'createandAddBeforefunctionIdJs',
+        functionJs: () => { createandAddBeforefunctionJs() },
+        jsText: '&lt;ul id="menujs"&gt; <br>   &ensp;&lt;li&gt;Services&lt;/li&gt; <br>  &ensp;&lt;li&gt;About &lt;/li&gt; <br>  &ensp;&lt;li&gt;Contact&lt;/li&gt; <br> &lt;/ul&gt; <br>  menu=document.getElementById("menujs"); <br>  li = document.createElement("li"); <br> li.innerText= "First This" <br> menu.insertBefore(li,menu.firstElementChild); ',
+        IdTestResultjs: 'createandAddBeforeIdTestResultjs',
+        functionIdJq: 'createandAddBeforefunctionIdJq',
+        functionJq: () => { createandAddBeforefunctionJq() },
+        jqText: 'content = "&lt;ulid="menujq"&gt;<br>&ensp;&lt;li id="liFirst" &gt;Services&lt;/li&gt; <br>&ensp; &lt;li&gt;About&lt;/li&gt; <br>&ensp; &lt;li&gt;"Contact&lt;/li&gt; <br> &lt;/ul&gt; <br> li=$("&lt;li&gt;&lt;/li&gt;").text("First This"); li.insertBefore($("#liFirst")) mainjq.html(content);',
+        IdTestResultjq: 'createandAddBeforeIdTestResultjq'
+    },
+    {
+        title: `Create and Add  Element After`,
+        idButtonClick: 'createandAddAfteridButtonClick',
+        divdisplay: 'createandAddAfterdivdisplay',
+        functionIdJs: 'createandAddAfterfunctionIdJs',
+        functionJs: () => { createandAddAfterfunctionJs() },
+        jsText: '&lt;ul id="menujs"&gt; <br>   &ensp;&lt;li&gt;Services&lt;/li&gt; <br>  &ensp;&lt;li&gt;About &lt;/li&gt; <br>  &ensp;&lt;li&gt;Contact&lt;/li&gt; <br> &lt;/ul&gt; <br>  menu=document.getElementById("menujs"); <br>  li = document.createElement("li"); <br> li.innerText= "Second This" <br> menu.firstElementChild.after(li); ',
+        IdTestResultjs: 'createandAddAfterIdTestResultjs',
+        functionIdJq: 'createandAddAfterfunctionIdJq',
+        functionJq: () => { createandAddAfterfunctionJq() },
+        jqText: 'content = "&lt;ulid="menujq"&gt;<br>&ensp;&lt;li id="liFirst" &gt;Services&lt;/li&gt; <br>&ensp; &lt;li&gt;About&lt;/li&gt; <br>&ensp; &lt;li&gt;"Contact&lt;/li&gt; <br> &lt;/ul&gt; <br> li=$("&lt;li&gt;&lt;/li&gt;").text("First This"); li.insertAfter($("#liFirst")) mainjq.html(content);',
+        IdTestResultjq: 'createandAddAfterIdTestResultjq'
+    },
+    {
+        title: `Create clone`,
+        idButtonClick: 'cloneidButtonClick',
+        divdisplay: 'clonedivdisplay',
+        functionIdJs: 'clonefunctionIdJs',
+        functionJs: () => { clonefunctionJs() },
+        jsText: 'let pOne = document.createElement("p") <br> let newElement = pOne.cloneNode(true);',
+        IdTestResultjs: 'cloneIdTestResultjs',
+        functionIdJq: 'clonefunctionIdJq',
+        functionJq: () => { clonefunctionJq() },
+        jqText: 'let el = $("&lt;p&gt;"+"This is a P "+"&lt;/p&gt;"); <br> let newElement = $(el).clone()',
+        IdTestResultjq: 'cloneIdTestResultjq'
+    },
+    {
+        title: `Add Class`,
+        idButtonClick: 'addClassidButtonClick',
+        divdisplay: 'addClassdivdisplay',
+        functionIdJs: 'addClassfunctionIdJs',
+        functionJs: () => { addClassfunctionJs() },
+        jsText: 'let pOne = document.createElement("p") <br> pOne.classList.add("pClassGreen")',
+        IdTestResultjs: 'addClassIdTestResultjs',
+        functionIdJq: 'addClassfunctionIdJq',
+        functionJq: () => { addClassfunctionJq() },
+        jqText: 'let el = $("&lt;p&gt;"+"This is a P "+"&lt;/p&gt;"); <br> $(el).addClass("pClassGreen")',
+        IdTestResultjq: 'addClassIdTestResultjq'
+    },
+    {
+        title: `Remove Class`,
+        idButtonClick: 'removeClassidButtonClick',
+        divdisplay: 'removeClassdivdisplay',
+        functionIdJs: 'removeClassfunctionIdJs',
+        functionJs: () => { removeClassfunctionJs() },
+        jsText: 'let pOne = document.createElement("p") <br> pOne.classList.remove("pClassGreen")',
+        IdTestResultjs: 'removeClassIdTestResultjs',
+        functionIdJq: 'removeClassfunctionIdJq',
+        functionJq: () => { removeClassfunctionJq() },
+        jqText: 'let el = $("&lt;p&gt;"+"This is a P"+"&lt;/p&gt;"); <br> $(el).removeClass("pClassGreen")',
+        IdTestResultjq: 'removeClassIdTestResultjq'
+    },
+    {
+        title: `Toggle Class`,
+        idButtonClick: 'toggleClassidButtonClick',
+        divdisplay: 'toggleClassdivdisplay',
+        functionIdJs: 'toggleClassfunctionIdJs',
+        functionJs: () => { toggleClassfunctionJs() },
+        jsText: 'let pOne = document.createElement("p") <br>  pOne.classList.toggle("pClassGreen");',
+        IdTestResultjs: 'toggleClassIdTestResultjs',
+        functionIdJq: 'toggleClassfunctionIdJq',
+        functionJq: () => { toggleClassfunctionJq() },
+        jqText: 'let el = $("&lt;p&gt;"+"This is a P "+"&lt;/p&gt;"); <br>  $(el).toggleClass("pClassGreen")',
+        IdTestResultjq: 'toggleClassIdTestResultjq'
+    },
+    {
+        title: `Add Disabled Attribute `,
+        idButtonClick: 'addDisabledidButtonClick',
+        divdisplay: 'addDisableddivdisplay',
+        functionIdJs: 'addDisabledfunctionIdJs',
+        functionJs: () => { addDisabledfunctionJs() },
+        jsText: "let btnTwo= document.getElementById('AddDisabledjs') <br> btnTwo.disabled = true; ",
+        IdTestResultjs: 'addDisabledIdTestResultjs',
+        functionIdJq: 'addDisabledfunctionIdJq',
+        functionJq: () => { addDisabledfunctionJq() },
+        jqText: " $('#addDisabledjq').attr('disabled', true)",
+        IdTestResultjq: 'addDisabledIdTestResultjq'
+    },
+    {
+        title: `Remove Disabled Attribute `,
+        idButtonClick: 'removeDisabledidButtonClick',
+        divdisplay: 'removeDisableddivdisplay',
+        functionIdJs: 'removeDisabledfunctionIdJs',
+        functionJs: () => { removeDisabledfunctionJs() },
+        jsText: "let btnTwo= document.getElementById('removeDisabledjs') <br> btnTwo.disabled = false; ",
+        IdTestResultjs: 'removeDisabledIdTestResultjs',
+        functionIdJq: 'removeDisabledfunctionIdJq',
+        functionJq: () => { removeDisabledfunctionJq() },
+        jqText: " $('#removeDisabledjq').attr('disabled', false)",
+        IdTestResultjq: 'removeDisabledIdTestResultjq'
+    },
+    {
+        title: `Set Data-src Attribute `,
+        idButtonClick: 'setDataSrcidButtonClick',
+        divdisplay: 'setDataSrcdivdisplay',
+        functionIdJs: 'setDataSrcfunctionIdJs',
+        functionJs: () => { setDataSrcfunctionJs() },
+        jsText: "let btnTwo= document.getElementById('setDataSrcjs') <br> btnTwo.disabled = true; ",
+        IdTestResultjs: 'setDataSrcIdTestResultjs',
+        functionIdJq: 'setDataSrcfunctionIdJq',
+        functionJq: () => { setDataSrcfunctionJq() },
+        jqText: " $('#setDataSrcjq').attr('disabled', true)",
+        IdTestResultjq: 'setDataSrcIdTestResultjq'
     },
 ]
 
 function pepe() {
-    allSelectors.forEach(function (elem) {
+    allFunctions.forEach(function (elem) {
         createDiv(elem.title, elem.idButtonClick, elem.divdisplay, elem.functionIdJs, elem.functionJs, elem.jsText, elem.IdTestResultjs, elem.functionIdJq, elem.functionJq, elem.jqText, elem.IdTestResultjq)
     })
 
-    allSelectors.forEach(function (elem) {
+    allFunctions.forEach(function (elem) {
         let divdisplay = document.getElementById(elem.divdisplay)
 
         let showButton = document.getElementById(elem.idButtonClick)
@@ -88,10 +231,10 @@ function hideFunc() {
         }
 }
 
-function buttonTest(father) {
+function buttonTest(father, id) {
     var btn = document.createElement('button')
     btn.textContent = 'Button Test'
-    btn.setAttribute('id', 'button-test')
+    btn.setAttribute('id', id)
     btn.classList.add('btn')
     btn.classList.add('btn-danger')
     divtest = document.getElementById(father)
--- assets/scripts/scriptJs.js
+++ assets/scripts/scriptJs.js
@@ -1,13 +1,169 @@
 function createElementfunctionJs  () {
+    var div = document.getElementById("createElementIdTestResultjs");
     el = document.createElement("span");
     el.innerHTML = "text";
-    var div = document.getElementById("createElementIdTestResultjs");
     div.appendChild(el);
 }
+
 function removeElementfunctionJs () {
-    buttonTest('removeElementIdTestResultjs')
+    buttonTest('removeElementIdTestResultjs', "button-testjs")
     setTimeout(() => {
-        var myobj = document.getElementById("button-test");
+        var myobj = document.getElementById("button-testjs");
         myobj.remove();
     }, 3000);
+}
+
+function appendElementfunctionJs() {
+    var main = document.getElementById('appendElementIdTestResultjs')
+    let pOne = document.createElement("p")
+    main.appendChild(pOne)
+    pOne.innerText= 'This is a P'
+    setTimeout(() => {
+        let span = document.createElement("span")
+        pOne.appendChild(span);
+        span.innerText= 'this is a Span'
+    }, 3000);
+}
+
+function prependElementfunctionJs() {
+    var main = document.getElementById('prependElementIdTestResultjs')
+    let pOne = document.createElement("p")
+    main.appendChild(pOne)
+    pOne.innerText= 'This is a P'
+    setTimeout(() => {
+        let span = document.createElement("span")
+        pOne.prepend(span);
+        span.innerText= 'this is a Span'
+    }, 3000);
+}
+function createandAddBeforefunctionJs () {
+    let main = document.getElementById('createandAddBeforeIdTestResultjs')
+    let content = '<ul id="menujs"><li>Services</li><li>About</li><li>Contact</li></ul>'
+    main.innerHTML = content
+    let menu= document.getElementById('menujs')
+    setTimeout(() => {
+        li = document.createElement("li");
+        li.innerText= 'First This'
+        menu.insertBefore(li, menu.firstElementChild);
+        }, 3000);
+}
+
+function createandAddAfterfunctionJs () {
+    let main = document.getElementById('createandAddAfterIdTestResultjs')
+    let content = '<ul id="menujs1" ><li>Services</li><li>About</li><li>Contact</li></ul>'
+    main.innerHTML = content
+    let menu= document.getElementById('menujs1')
+    setTimeout(() => {
+        let li = document.createElement("li");
+        li.innerHTML= 'Second This'
+        menu.firstElementChild.after(li);
+        }, 3000);
+}
+
+function clonefunctionJs() {
+    var mainclone = document.getElementById('cloneIdTestResultjs')
+    let pOne = document.createElement("p")
+    mainclone.appendChild(pOne)
+    pOne.innerText= 'This is a P'
+    setTimeout(() => {
+        let newElement = pOne.cloneNode(true);
+        newElement.innerText= 'This is a P is a clone'
+        mainclone.appendChild(newElement)
+    }, 3000);
+}
+
+function addClassfunctionJs() {
+    var mainclone = document.getElementById('addClassIdTestResultjs')
+    let pOne = document.createElement("p")
+    mainclone.appendChild(pOne)
+    pOne.innerText= 'This is a P '
+    setTimeout(() => {
+        pOne.innerText= 'This is a P with class'
+        pOne.classList.add('pClassGreen')
+    }, 3000);
+}
+
+function removeClassfunctionJs() {
+    var main = document.getElementById('removeClassIdTestResultjs')
+    let pOne = document.createElement("p")
+    main.appendChild(pOne)
+    pOne.innerText= 'This is a P with class '
+    pOne.classList.add('pClassGreen')
+    setTimeout(() => {
+        pOne.innerText= 'This is a P wihtout class'
+        pOne.classList.remove('pClassGreen')
+
+    }, 3000);
+}
+
+function toggleClassfunctionJs() {
+    buttonTest('toggleClassIdTestResultjs', 'togglebtnjs')
+    btn= document.getElementById('togglebtnjs')
+    var main = document.getElementById('toggleClassIdTestResultjs')
+    let pOne = document.createElement("p")
+    main.appendChild(pOne)
+    pOne.innerText= 'This is a P '
+    btn.addEventListener('click', function () {
+        pOne.classList.toggle('pClassGreen')
+    } )
+}
+
+function addDisabledfunctionJs() {
+    buttonTest('addDisabledIdTestResultjs', 'addDisabledbtn')
+    buttonTest('addDisabledIdTestResultjs', 'AddDisabledjs')
+
+    btn= document.getElementById('addDisabledbtn')
+    let btnTwo= document.getElementById('AddDisabledjs')
+
+    btnTwo.addEventListener('click', function () {
+        let pOne = document.createElement("p")
+        btnTwo.after(pOne)
+        pOne.innerText= 'This is a P '
+    })
+    btn.addEventListener('click', function () {
+        document.getElementById('AddDisabledjs').disabled = true;
+    } )
+}
+
+function removeDisabledfunctionJs() {
+    buttonTest('removeDisabledIdTestResultjs', 'removeDisabledbtn')
+    buttonTest('removeDisabledIdTestResultjs', 'removeDisabledjs')
+
+    btn= document.getElementById('removeDisabledbtn')
+    let btnTwo= document.getElementById('removeDisabledjs')
+
+    btnTwo.disabled = true;
+    btnTwo.addEventListener('click', function () {
+        let pOne = document.createElement("p")
+        btnTwo.after(pOne)
+        pOne.innerText= 'This is a P '
+    })
+    btn.addEventListener('click', function () {
+        btnTwo.disabled = false;
+    } )
+}
+
+
+function setDataSrcfunctionJs() {
+    buttonTest('setDataSrcIdTestResultjs', 'setDataSrcbtn')
+    //buttonTest('setDataSrcIdTestResultjs', 'removeDisabledjs')
+
+    btn= document.getElementById('setDataSrcbtn')
+    //let btnTwo= document.getElementById('removeDisabledjs')
+
+    let image = document.createElement("img")
+    image.className = 'my-img';
+    image.src = '/assets/css/img/10592-EGO-LV1-6-Planet-Eclipse--68-Cal-Fire-Opal-gold---rot.jpg';
+    document.body.appendChild();
+        btnTwo.after(pOne)
+        pOne.innerText= "Don't have Data-src"
+    
+    //     btnTwo.addEventListener('click', function () {
+    //     let pOne = document.createElement("p")
+    //     btnTwo.after(pOne)
+    //     pOne.innerText=
+    // })
+    btn.addEventListener('click', function () {
+        
+    } )
 }
