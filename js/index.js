0296fea31d2aa113d2b23567d8d3b8a098e50b0b
Luis Molina <luismateo2416@gmail.com>
2021-11-28 12:16:59 +0100

    all vanilla js exercises

--- js/index.js
+++ js/index.js
@@ -3,14 +3,18 @@
 let eventsList=[];
 let functionsList=[];
 let selectorsList=[];
+var times=0;
+var timesquery=1;
 //Modal creation
 function createModal(obj){
+    times=0;
+    timesquery=1;
     $("body").append(`
     
                 <div id="background-modal"></div>                  
                     <div id="modal">
                         <header class="">
-                            ${obj.group}
+                            <h2>${obj.group}</h2>
                             <img id="exit" src="../assets/img/cross.png">
                         </header>
 
@@ -45,7 +49,7 @@ function createModal(obj){
                                     $("#exit").on("click", removeModal);
                                     $("#background-modal").on("click",removeModal);
                                     //create button example
-                                    const buttonVanilla=$("<button id='vanillaBtn'>Press me to see how it works!</</button>");
+                                    const buttonVanilla=$("<button id='vanillaBtn'>Press me to see how it works!</button>");
                                     $(buttonVanilla).one("click",obj.vanillaMethod);
                                     const buttonJquery=$("<button id='jqueryBtn'>Press me to see how it works!</button>");
                                     $(buttonJquery).one("click",obj.jqueryMethod);
@@ -73,22 +77,29 @@ $(function(){
 //TODO hacer funcion que filtre el nombre del target y encuentre el objeto en el array
 function filterElement(element){
     //pick title of the section
-    const grandFather=getTitle("#events");
+    const grandFather=getTitle("#"+$(element).parent().parent().attr("id"));
     if(grandFather=="Events"){
         const objFiltered=eventsList.find(obj=>obj.name==element.textContent);
         return objFiltered;
         
+    }else if(grandFather=="Functions"){
+        const objFiltered=functionsList.find(obj=>obj.name==element.textContent);
+        return objFiltered;
+
+    }else if(grandFather=="Selectors"){
+        const objFiltered=selectorsList.find(obj=>obj.name==element.textContent);
+        return objFiltered;
     }
 }
 
 //TODO convert items to obj and put all in array of obj
 function createItemObj(){
     const eventsItems=$("#events").children(".content").children();
-    const title=getTitle("#events");
-    console.log(title);
+    const titleEvents=getTitle("#events");
+    //Events
     eventsItems.each(function(idx,element){
         eventsList[idx]={
-            group: title,
+            group: titleEvents,
             name:element.textContent,
             vanillaMethod:undefined,
             jqueryMethod:undefined,
@@ -97,6 +108,46 @@ function createItemObj(){
 
         }
     })
+    //functions
+    const functionsItems=$("#functions").children(".content").children();
+    const titleFunctions=getTitle("#functions");
+    functionsItems.each(function(idx,element){
+        functionsList[idx]={
+            group: titleFunctions,
+            name:element.textContent,
+            vanillaMethod:undefined,
+            jqueryMethod:undefined,
+            vanillaSnippet:undefined,
+           jQuerySnippet:undefined,
+        }
+    })
+    //functions 2
+    const functions2Items=$("#functions2").children(".content").children();
+    const titleFunctions2=getTitle("#functions2");
+    functions2Items.each(function(idx,element){
+        functionsList.push({
+            group: titleFunctions2,
+            name:element.textContent,
+            vanillaMethod:undefined,
+            jqueryMethod:undefined,
+            vanillaSnippet:undefined,
+           jQuerySnippet:undefined,
+        })
+    });
+    //selectors
+    const selectorsItems=$("#selectors").children(".content").children();
+    const titleSelectors=getTitle("#selectors");
+    selectorsItems.each(function(idx,element){
+        selectorsList[idx]={
+            group: titleSelectors,
+            name:element.textContent,
+            vanillaMethod:undefined,
+            jqueryMethod:undefined,
+            vanillaSnippet:undefined,
+           jQuerySnippet:undefined,
+        }
+    })
+
 }
 createItemObj();
 
--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,185 @@
+//HTML document has been loaded
 function htmlDocumentLoadedjquery(){
-    $("#jqueryBtn").parent().append(`<p>The document is loaded!</p>`)
+    $(".demo").eq(1).append(`<p>The document is loaded!</p>`)
 }
 eventsList[0].jqueryMethod=function(){
     htmlDocumentLoadedjquery();
 }
-eventsList[0].jquerySnippet=`<code>$("document").ready(value)</code>`
+eventsList[0].jquerySnippet=`<code>$().on("ready",function(){})</code>`
+//HTML item has been clicked 
+function htmlItemClickedjquery(){
+    const demo=$(".demo").eq(1)
+    const btn=$(`<button>Click on me!</button>`)
+    const p=$(`<p></p>`)
+    
+    $(btn).on("click",function(){
+        p.text(`You clicked me ${timesquery} times`)
+        $(demo).append(p)
+        timesquery++;
+    })
+    $("#jqueryBtn").remove();
+    $(demo).append(btn);
+}
+eventsList[1].jqueryMethod=function(){
+    htmlItemClickedjquery();
+}
+eventsList[1].jquerySnippet=`<code>
+                                $("item").on
+                                ("click",function(){})
+                            </code>`
+//item has benn double clicked
+function htmlItemDoubleClickedjquery(){
+    const demo=$(".demo").eq(1)
+    const btn=$(`<button>Double click on me!</button>`)
+    const p=$(`<p></p>`)
+    
+    $(btn).on("dblclick",function(){
+        p.text(`You clicked me ${timesquery} times`)
+        $(demo).append(p)
+        timesquery++;
+    })
+    $("#jqueryBtn").remove();
+    $(demo).append(btn);
+}
+eventsList[2].jqueryMethod=function(){
+    htmlItemDoubleClickedjquery();
+}
+eventsList[2].jquerySnippet=`<code>
+                                $("item").on
+                                ("dblclick",function(){})
+                            </code>`
+//user presses a key on the keyboard 
+function pressKeyjquery(){
+    const demo=$(".demo").eq(1)
+    const btn=$(`<button>Press a key!</button>`)
+    const p=$(`<p></p>`)
+    
+    btn.on("keypress",function(e){
+        p.text(`You pressed the ${e.key} key`)
+        $(demo).append(p)
+    })
+    $("#jqueryBtn").remove();
+    demo.append(btn);
+}
+eventsList[3].jqueryMethod=function(){
+    pressKeyjquery();
+}
+eventsList[3].jquerySnippet=`<code>
+                                $("item").on
+                                ("keypress",function(){})
+                            </code>`
+//user moves the mouse cursor
+function movesCursorjquery(){
+    const demo=$(".demo").eq(1)
+    const btn=$(`<button>Move the mouse</button>`)
+    const p=$(`<p></p>`)
+
+    $(btn).on("mousemove",function(){
+        p.text(`You moved the mouse!`)
+        $(demo).append(p)
+    })
+    $("#jqueryBtn").remove();
+    demo.append(btn);
+}
+eventsList[4].jqueryMethod=function(){
+    movesCursorjquery();
+}
+eventsList[4].jquerySnippet=`<code>
+                            $("item").on
+                            ("mousemove",function(){})
+                        </code>`
+//user changes a value of an text input
+function changeValueInputjquery(){
+    const demo=$(".demo").eq(1)
+    const btn=$(`<button>Write something!</button>`)
+    const p=$(`<p></p>`)
+    const input=$("<input>");
+
+    input.on("change",function(){
+        p.text(`You changed the input to ${input.val()}!`)
+        demo.append(p)
+    })
+    $("#jqueryBtn").remove();
+    demo.append(btn);
+    demo.append(input);
+}
+eventsList[5].jqueryMethod=function(){
+    changeValueInputjquery();
+}
+eventsList[5].jquerySnippet=`<code>
+                            $("item").on
+                            ("change",function(){})
+                            p.text("You changed the value to "+input.val())!)
+                        </code>`
+//image is loaded
+function imageIsLoadedjquery(){}
+//image fails to load
+function imageFailsjquery(){}
+//form is submitted
+function formSubmitjquery(){}
+//user changes the option of a select element
+function changeValueSelectjquery(){}
+//When you position the mouse over an element
+function mouseOverjquery(){}
+//When a checkbox is checked or unchecked
+function checkboxjquJquery(){}
+//When a ul list item is clicked, show the item that was clicked
+function itemListClickedjquery(){}
+//Functions section
+//Create an HTML element
+function createHtmlElementJquery(){}
+//Remove an HTML element
+function removeHtmlElementJquery(){}
+//Append an HTML element
+function appendHtmlElementJquery(){}
+//Prepend an HTML element 
+function prependHtmlElementJquery(){}
+//Create and add an HTML element after another element 
+function createAfterHtmlElementJquery(){}
+//Create and add an HTML element before another element 
+function createBeforeHtmlElementJquery(){}
+//Clone an HTML element within other element
+function cloneHtmlElementJquery(){}
+//Add a class to an HTML item
+function addClassHtmlElementJquery(){}
+//Remove a class to an HTML item
+function removeClassHtmlElementJquery(){}
+//Toggle a class of an HTML item
+function toggleClassHtmlElementJquery(){}
+//Add a disabled attribute to an HTML button
+function disabledAttrHtmlElementJquery(){}
+//Remove the disabled attribute of an HTML button
+function removedisabledAttrHtmlElementJquery(){}
+//Set a data-src attribute to a img element
+function dataSrcHtmlElementJquery(){}
+//Remove the data-src attribute of the img element
+function removeDataSrcHtmlElementJquery(){}
+//Hide an HTML element on click (display: none)
+function hideHtmlElementJquery(){}
+//Show an HTML element on click (display: block)
+function blockHtmlElementJquery(){}
+//Fade in an HTML element using jQuery
+//Fade out an HTML element using jQuery
+//Animate an item after 2 seconds from the initial page load
+function animateHtmlElementJquery(){}
+//Selectors section
+//Iterate a collection of elements and apply a change of style on them
+function changeStyleHtmlElementJquery(){}
+//Get the parent element of a certain element and change its font weight
+function getParentHtmlElementJquery(){}
+//Get the collection of children of a certain element and change its font weight
+function collectionChildrenHtmlElementJquery(){}
+//Get all the elements that have a certain class and change their font weight
+function fontWeightOfCollectionHtmlElementJquery(){}
+//Get an item by id
+function getIdHtmlElementJquery(){}
+//Get all the elements that have a certain class and the display property of none 
+function hideCollectionHtmlElementJquery(){}
+//Get the options of a select element that are selected
+function getOptionsJquery(){}
+//Change an attribute of a first element
+function changeAttributeFirstElementHtmlElementJquery(){}
+//Show an alert with the value of the first input of the page
+function alertOnChangeInputJquery(){}
+//Remove all items from a specific selector
+function removeItemsFromSpecificSelectorJquery(){}
